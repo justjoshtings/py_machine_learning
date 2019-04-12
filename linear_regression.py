caf029f01cab80c88bb8175ccef5d7766ad55475
justjoshtings <joshting1@gmail.com>
2019-04-11 22:49:28 -0400

    Updates to linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,18 @@
 from statistics import mean
 import numpy as np
 import matplotlib as mpl
-mpl.use('TkAgg') #Python MacOSX backend ImportError fix
+mpl.use('TkAgg') #Python MacOS backend ImportError fix
 from matplotlib import pyplot as plt
 from matplotlib import style
 import random
 
 style.use('fivethirtyeight')
 
-# xs = np.array([1, 2, 3, 4, 5, 6, 7, 8], dtype=np.float64)
-# ys = np.array([3, 2, 5, 7, 5, 7, 9, 5], dtype=np.float64)
-
 def create_dataset(n, variance, step=1.5, correlation=False):
+	'''
+	Fn to create a random dataset for testing algorithm purposes.
+	Returns numpy arrays of X and Y values.
+	'''
 	val = 0
 	ys = []
 	for i in range(n):
@@ -24,38 +25,49 @@ def create_dataset(n, variance, step=1.5, correlation=False):
 	xs = [i for i in range(len(ys))]
 
 	return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
-# plt.scatter(xs, ys)
-# plt.show()
 
 def calc_slope_intercept(xs, ys):
+	'''
+	Fn to calculate the slope and intercepts of our dataset.
+	Returns the slope and intercept.
+	'''
 	m = ((mean(xs)*mean(ys)) - (mean(xs*ys)))/((mean(xs)*mean(xs)) - mean(xs*xs))
 	b = mean(ys) - m*mean(xs)
 	return m, b
 
 def squared_error(ys, regression_line):
-	e_square = sum((regression_line - ys)**2)
-	return e_square
+	'''
+	Fn to calculate the squared error of our dataset.
+	Return the squared error value.
+	'''
+	return sum((regression_line - ys)**2)
 
 def coeff_of_determination(ys, regression_line):
+	'''
+	Fn to calculate the Coefficienct of Determination or more commonly known as R value for our dataset.
+	Return R value.
+	'''
 	y_mean_line = [mean(ys) for y in ys]
 	squared_error_regression = squared_error(ys, regression_line)
 	squared_error_y_mean = squared_error(ys, y_mean_line)
 	return 1 - (squared_error_regression/squared_error_y_mean)
 
-
-xs, ys = create_dataset(50, 20, correlation=False)
+'''
+Initialize and run sample dataset and linear regression algorithm.
+Run a sample prediction.
+Plot dataset, best fit line, and prediction.
+'''
+xs, ys = create_dataset(50, 20, correlation='pos')
 
 m, b = calc_slope_intercept(xs, ys)
 
-# print(m, b)
-
 regression_line = [(m*x)+b for x in xs]
 
-predict_x = 4.5
+predict_x = 7
 predict_y = (m*predict_x)+b
 
 r_squared = coeff_of_determination(ys, regression_line)
-print(r_squared)
+print('The R value is: {:.2f}'.format(r_squared))
 
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color='g', s=100)
